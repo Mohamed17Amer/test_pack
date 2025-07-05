37be7456422f4be96d36a7a88634ec301d8749ec
Mohamed Abd EL-Moneim Amer <138235067+Mohamed17Amer@users.noreply.github.com>
2025-07-05 06:13:02 +0200

    retest

--- test_pack/lib/cores/utils/api_service.dart
+++ test_pack/lib/cores/utils/api_service.dart
@@ -1,8 +1,9 @@
 import 'dart:developer';
-
 import 'package:dio/dio.dart';
 import 'package:test_pack/features/posts/data/models/post_model.dart';
 import 'package:test_pack/features/posts/domain/entities/post_entity.dart';
+
+// remember to study headers and options   VIP
 
 //  To only fetch, not to parse or manipulate data
 // This service is responsible for making API calls and returning the raw data without any additional processing.
@@ -16,14 +17,7 @@ class ApiService {
   Future<List<dynamic>> get({required String endPoint}) async {
     var response = await _dio.get(
       '$baseUrl$endPoint',
-      options: Options(
-        headers: {
-          'User-Agent':
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-          'Accept':
-              'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        },
-      ),
+      options: optionsKareem(),
     );
     return response.data;
   }
@@ -38,14 +32,18 @@ class ApiService {
   }
 
   Future<void> addPost(PostEntity post) async {
-    final response = await _dio.post("$baseUrl/posts", data: post);
+    final response = await _dio.post(
+      "$baseUrl/posts",
+      data: post,
+      options: optionsKareem(),
+    );
     log("on addition ${response.data}");
   }
 
   Future<void> deletePosst(int id) async {
     final response = await _dio.delete(
       '$baseUrl/posts/$id',
-      options: Options(headers: {"Content-Type": "application/json"}),
+      options: optionsKareem(),
     );
 
     log("on deletion  ${response.data} ");
@@ -57,15 +55,23 @@ class ApiService {
       "$baseUrl/posts/${post.id}",
       data: post,
       //  options: Options(headers: {"Content-Type": "application/json"}),
-      options: Options(
-        headers: {
-          'User-Agent':
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-          'Accept':
-              'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        },
-      ),
+      options: optionsKareem(),
     );
     log("on update  ${response.data} ");
+  }
+
+  optionsOmran() {
+    return Options(headers: {"Content-Type": "application/json"});
+  }
+
+  optionsKareem() {
+    return Options(
+      headers: {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept':
+            'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      },
+    );
   }
 }
