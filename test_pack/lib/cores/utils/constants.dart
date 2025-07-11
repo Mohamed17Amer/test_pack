28b00302a550fabeefeb973b02555d13217037b6
Mohamed Abd EL-Moneim Amer <138235067+Mohamed17Amer@users.noreply.github.com>
2025-07-11 12:18:02 +0200

    REMOVE ALL FILES TO RESOLVE

--- test_pack/lib/cores/utils/constants.dart
+++
@@ -1,1 +0,0 @@
- const String   postsBox =  "posts_box";
--- test_pack/lib/features/auth_firebase/domain/repo/auth_with_mail.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:test_pack/cores/errors/failure.dart';
-
-abstract class AuthWithEmailRepository {
-  Future<Either<Failure, String>> signUpWithEmail(String email, String password);
-  Future<Either<Failure, String>> signInWithEmail(String email, String password);
-}
